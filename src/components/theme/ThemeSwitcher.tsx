cb185456a9b2f9fc088697773e2c24e081d0bcad
esirango <esirango23@gmail.com>
2025-08-14 14:30:54 +0330

    fix: theme switcher style

--- src/components/theme/ThemeSwitcher.tsx
+++ src/components/theme/ThemeSwitcher.tsx
@@ -27,21 +27,21 @@ export default function ThemeSwitcher() {
                 onClick={() => setDark(!dark)}
             >
                 {/* پایه و دسته چراغ */}
-                <div className="absolute bottom-2 left-[12px] w-1 h-5 bg-gray-600 origin-bottom -translate-x-1/2 rotate-30 z-10"></div>
-                <div className="absolute bottom-6 right-1.5 w-1 h-6 bg-gray-600 origin-bottom -translate-x-1/2 -rotate-30 z-10"></div>
+                <div className="absolute bottom-[14px] left-1.5 w-1 h-4 bg-gray-600 origin-bottom rotate-40 z-10"></div>
+                <div className="absolute bottom-6 right-[10px] w-1 h-4 bg-gray-600 origin-bottom -translate-x-1/2 -rotate-20 z-10"></div>
 
                 {/* کلاهک چراغ */}
                 <div
-                    className={`absolute bottom-10 left-1/2 w-6 h-3 rounded-t-full -translate-x-1/2 bg-gray-500 overflow-hidden
+                    className={`absolute bottom-10 left-2 w-6 h-3 rounded-t-full -translate-x-1/2 bg-gray-500 overflow-hidden rotate-30
             ${dark ? "animate-soft-sway z-10" : "z-10"}`}
                 ></div>
 
                 {/* بخش کوچک بالای کلاهک */}
-                <div className="absolute bottom-[7px] left-1/2 w-6 h-1 bg-gray-700 -translate-x-1/2 rounded z-10"></div>
+                <div className="absolute bottom-[12px] left-1.5 w-6 h-1 bg-gray-700 -translate-x-1/2 rounded z-10"></div>
 
                 {/* نور مات داخل کلاهک */}
                 <div
-                    className={`absolute left-1/2 w-3 h-1.5 rounded-t-full -translate-x-1/2
+                    className={`absolute left-0 w-3 h-1.5 rounded-t-full -translate-x-1/2
             ${
                 dark
                     ? "bg-yellow-300 opacity-40 blur-sm animate-light-flicker z-50"
@@ -51,7 +51,7 @@ export default function ThemeSwitcher() {
 
                 {/* فقط نیم‌دایره بیرون از کلاهک */}
                 <div
-                    className={`absolute top-[8px] left-1/2 w-3 h-1.5 rounded-b-full -translate-x-1/2
+                    className={`absolute top-[6.5px] left-[3px] w-3 h-1.5 rotate-29 rounded-b-full -translate-x-1/2
             ${
                 dark
                     ? "bg-yellow-400 opacity-100 shadow-[0_0_6px_2px_rgba(255,255,0,0.5)] animate-light-flicker z-50"
